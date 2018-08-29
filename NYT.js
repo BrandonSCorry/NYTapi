0c431eed2a849bbbfa6988ece4b22dc2d94bc98a
Poltergeist <brandonssc@gmail.com>
2018-08-29 18:08:23 -0400

    fixing js

--- NYT.js
+++ NYT.js
@@ -4,31 +4,36 @@ $("#search-submit").on("click", function() {
     event.preventDefault();
     $("#nyt-dump").empty();
     var search = $("#term").val();
-    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + search + "&api-key=c43fdcb0dd074a0bb172684363bdf7bd";
-    console.log(search);
-    console.log(url);
-    $.ajax({
+  var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  url += '?' + $.param({
+    'api-key': "c43fdcb0dd074a0bb172684363bdf7bd",
+    'q': search
+  });
+  $.ajax({
     url: url,
     method: 'GET',
-    }).then(function(result) {
+  }).done(function(result) {
     console.log(result);
-    console.log(search);
     var search_results = result.response.docs;
 
     for (var i = 0; i < search_results.length; i++){
-        var div = $("<div>");
-        var p = $("<p>");
-        var headline = search_results[i].headline.main;
-        var snippet = search_results[i].snippet;
-        console.log(headline);
-        
-        
+      var div = $("<div>");
+      var p = $("<p>");
+      var headline = search_results[i].headline.main;
+      var snippet = search_results[i].snippet;
+      console.log(headline);
 
 
-        $("#nyt-dump").append("<strong>" + headline + "</strong>" + "<br>");
-        $("#nyt-dump").append(snippet + "<br>" + "<br>");
+
+
+      $("#nyt-dump").append("<strong>" + headline + "</strong>" + "<br>");
+      $("#nyt-dump").append(snippet + "<br>" + "<br>");
     }
+  }).fail(function(err) {
+    throw err;
+  });    console.log(search);
+    console.log(url);
+
 
   });
-});
 });
